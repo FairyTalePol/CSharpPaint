cb4663cf49ba526c1e37f2d9c895ce51bc389a71
FairyTale <egorusdnepr@gmail.com>
2021-05-13 14:55:20 +0300

    BLM

--- FinalPaint/FinalPaint/Classes/Figure.cs
+++ FinalPaint/FinalPaint/Classes/Figure.cs
@@ -18,6 +18,7 @@ namespace FinalPaint.Classes
         //uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu
         //comment in new branch
         //comment for master
+        //syka blyat
         protected Graphics _g { get; set; }
         protected Pen _p { get; set; }
         protected Point _start { get; set; }
