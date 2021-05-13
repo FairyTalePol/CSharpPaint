7484f1826bffabba1d182be4deb40d9819446763
FairyTale <egorusdnepr@gmail.com>
2021-05-13 13:17:41 +0300

    test

--- FinalPaint/FinalPaint/Classes/Figure.cs
+++ FinalPaint/FinalPaint/Classes/Figure.cs
@@ -10,6 +10,7 @@ namespace FinalPaint.Classes
     abstract class Figure
     {
         //в конструктор передавать Graphics g, Pen p, Point start
+        //коммент что б проверить коммит
         protected Graphics _g { get; set; }
         protected Pen _p { get; set; }
         protected Point _start { get; set; }
