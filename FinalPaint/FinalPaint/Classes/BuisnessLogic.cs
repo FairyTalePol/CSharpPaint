52a82a42d630ee84bfa8be643814036946eb4a69
FairyTale <egorusdnepr@gmail.com>
2021-05-25 15:01:15 +0300

    minor fixes

---
+++ FinalPaint/FinalPaint/Classes/BuisnessLogic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalPaint.Classes
+{
+    class BuisnessLogic
+    {
+        private static BuisnessLogic _bl;
+        private BuisnessLogic()
+        {
+
+        }
+
+        public BuisnessLogic Create()
+        {
+            if (_bl==null)
+            {
+                _bl = new BuisnessLogic();
+            }
+            return _bl;
+        }
+
+
+    }
+}
