763053aadb8773e204e1a82f902a476e93a52d33
Pavel <pavelbig9999@gmail.com>
2021-05-25 15:54:17 +0300

    Added Config class

---
+++ FinalPaint/FinalPaint/Classes/Config.cs
@@ -0,0 +1,30 @@
+﻿using FinalPaint.Enum;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalPaint.Classes
+{
+    public static class Config
+    {
+        public static Dictionary<int, int> penWidth = new Dictionary<int, int>();
+        public static Pen pen = new Pen(Color.Black, 3);
+        public static SolidBrush brush = new SolidBrush(Color.Black);
+        public static int dropDownPenWidth = 0;
+        public static EButtonDrawingType eButtonDrawingType = EButtonDrawingType.Curve;
+        static Config()
+        {
+            penWidth.Add(0, 1);
+            penWidth.Add(1, 5);
+            penWidth.Add(2, 10);
+            penWidth.Add(3, 20);
+            pen.StartCap = LineCap.Round;
+            pen.EndCap = LineCap.Round;
+        }
+
+    }
+}
