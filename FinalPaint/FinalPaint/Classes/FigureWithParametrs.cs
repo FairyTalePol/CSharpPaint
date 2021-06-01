339b6c8013da0f889d8c5e6c581219d7f00fcf83
Pavel <pavelbig9999@gmail.com>
2021-06-01 14:46:37 +0300

    Added new class FiguresWithParametrs

---
+++ FinalPaint/FinalPaint/Classes/FigureWithParametrs.cs
@@ -0,0 +1,16 @@
+﻿namespace FinalPaint.Classes
+{
+    public class FigureWithParametrs
+    {
+        private Figure _figure;
+        private string _penColor;
+        private int _penSize;
+        
+        private void Figure(Figure figure, string penColor, int penSize)
+        {
+            _figure = figure;
+            _penColor = penColor;
+            _penSize = penSize;
+        }
+    }
+}
