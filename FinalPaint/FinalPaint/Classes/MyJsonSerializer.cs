54bbe8655078abbf667b466d50568c061f6d54a7
Pavel <pavelbig9999@gmail.com>
2021-06-01 13:55:23 +0300

    Add json serializer

---
+++ FinalPaint/FinalPaint/Classes/MyJsonSerializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace FinalPaint.Classes
+{
+    public class MyJsonSerializer
+    { 
+        string Serialize_(List<Figure> figures)
+        {
+            string json = JsonSerializer.Serialize(figures);
+            return json;
+        }
+        List<Figure> Deserilize_(string json)
+        {
+            List<Figure> figures = new List<Figure>();
+            figures = JsonSerializer.Deserialize<List<Figure>>(json);
+            return figures;
+        }
+    }
+}
